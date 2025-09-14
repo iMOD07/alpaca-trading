1c6621ced6b82321790e910b6f44ee2459dd642d
iMOD07 <mo.2301@hotmail.com>
2025-09-14 13:06:17 +0300

    add test

---
+++ src/main/java/com/alpaca/trading/MyProjectApplication.java
@@ -0,0 +1,16 @@
+package com.alpaca.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectApplication.class, args);
+
+        System.out.print("FirstProject");
+        System.out.print("Test Add");
+
+	}
+}
